fe70e00ec51483631839eca416492ad75cdc492f
gtjames <gtjames@gmail.com>
2023-11-13 13:46:49 -0600

    new Pool IDs

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-1_6Njhn1oi1',          // e.g. us-east-1_PZ1irZLMc
-        userPoolClientId:   '49d2qubnhtndnml5chlf25f1r',   // e.g. 56kf3jkb80icko329g1koap8f6
+        userPoolId:        'us-east-1_OcE0QEkMG',    // 'us-east-1_6Njhn1oi1',          // e.g. us-east-1_PZ1irZLMc
+        userPoolClientId:  '3uurcfoq1mp5do27atuk64c9ep', // '49d2qubnhtndnml5chlf25f1r',   // e.g. 56kf3jkb80icko329g1koap8f6
         region:             'us-east-1'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'https://7hkr3oyeg4.execute-api.us-east-1.amazonaws.com/dev'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:         '',// 'https://7hkr3oyeg4.execute-api.us-east-1.amazonaws.com/dev'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
